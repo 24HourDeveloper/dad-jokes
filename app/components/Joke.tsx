997d7eb7af89b3e39a9a8ae4b1aa435e77ee3c6b
24HourDeveloper <mikeitt009@gmail.com>
2024-07-16 14:39:07 -0400

    added new column, cleaned up some code, added server action, added prisma client singleton

---
+++ app/components/Joke.tsx
@@ -0,0 +1,81 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import { Heading, Flex, Card, Text, Button } from "@chakra-ui/react";
+import CardButtons from './CardButtons'
+
+type Joke = {
+  question: string;
+  answer: string;
+  xata_id: string;
+  laughs: number
+}
+type Jokes = {
+  jokes: Joke[]
+}
+export default function Joke({ jokes } : Jokes) {
+  
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [randomJoke, setRandomJoke] = useState<Joke | null>(null)
+
+  useEffect(() => {
+    const joke = jokes[Math.floor(Math.random() * 90)];
+    setRandomJoke(joke)
+  },[])
+
+  if(!randomJoke) return null;
+
+  return (
+    <Flex
+      flexDir="column"
+      gap="2"
+      w="100%"
+      justifyContent="center"
+      alignItems="center"
+      mt="6"
+    >
+      <Card
+        bg="orange.100"
+        p="4"
+        w={{ base: "95%", lg: "500px" }}
+        borderBottom="2px"
+        borderColor="orange"
+      >
+        <Flex flexDirection="column" gap="4">
+          <Heading size="lg">{randomJoke.question}</Heading>
+          {
+            !showAnswer && 
+            <Button
+              onClick={() => setShowAnswer(true)}
+              bg="orange.100"
+              _hover={{
+                color: "white",
+                bg: "orange.500",
+              }}
+            >
+              <Text textAlign="center">Click to see answer</Text>
+            </Button>
+          }
+        </Flex>
+      </Card>
+      {
+        showAnswer &&
+        <Card
+          p="4"
+          borderBottom="2px"
+          borderColor="orange"
+          w={{ base: "95%", lg: "500px" }}
+        >
+          <Text fontSize="xl" textAlign="center">{randomJoke.answer}</Text>
+        </Card>
+      }
+      <CardButtons
+        {...randomJoke}
+        disableLaugh={!showAnswer}
+        onNext={() => {
+          const joke = jokes[Math.floor(Math.random() * 90)];
+          setShowAnswer(false)
+          setRandomJoke(joke)
+        }}/>
+    </Flex>
+  )
+}
